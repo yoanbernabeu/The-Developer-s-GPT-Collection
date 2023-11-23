e01cd752558a32c81dc4cece07aaf7ccabf68598
Yoan Bernabeu <yoan.bernabeu@gmail.com>
2023-11-23 11:45:00 +0100

    Add new images and update index page

--- public/assets/resize.sh
+++ public/assets/resize.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
 # Répertoire contenant les images PNG
-input_directory="."
-
-# Répertoire pour stocker les images originales
-backup_directory="${input_directory}/sources"
-
-# Création du répertoire de sauvegarde s'il n'existe pas
-mkdir -p "$backup_directory"
+input_directory="./sources"
+output_directory="."
 
 # Boucle pour traiter chaque fichier PNG dans le répertoire
 for image in "${input_directory}"/*.png; do
-    # Copie de l'image originale dans le répertoire de sauvegarde
-    cp "$image" "$backup_directory"
-
-    # Réduction de la taille de l'image à 40x40 pixels
-    convert "$image" -resize 100x100 "$image"
+    # Réduction de la taille de l'image à 40x40 pixels et copie dans le répertoire de sortie
+    convert "${image}" -resize 100x100 "${output_directory}/$(basename "${image}")"
 done
 
 echo "Traitement terminé."
